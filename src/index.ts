aa05ab47e03086ec5c0d3b32f4db992a28baa647
cleitonrs <rscleitonrs@gmail.com>
2022-01-25 13:38:59 -0300

    CRUD done

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import usersRoute from './routes/users.route'
 
 const app = express()
 
+//  App Routes setups
 app.use(usersRoute)
 
+// App Setups
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Server init
 app.listen(3000, () => {
   console.log("Server running on Port: 3000")
 })
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response, NextFunction, Router} from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 // get /users
 // get/users/:uuid
@@ -10,7 +11,31 @@ const usersRoute = Router()
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Cleiton'}]
-  res.status(200).send(users)
+  res.status(StatusCodes.OK).send(users)
+})
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid
+  res.status(StatusCodes.OK).send({ uuid })
+})
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body
+
+  console.log(req.body)
+  res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid
+  const modifiedUser = req.body
+
+  modifiedUser.uuid = uuid
+  res.status(StatusCodes.OK).send(modifiedUser)
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  res.send(StatusCodes.OK)
 })
 
 export default usersRoute
